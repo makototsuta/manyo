f4c1c1266a83742120ee2a736b8efdbcad1cedca
makoto tsuta <makoto1850@gmail.com>
2020-04-05 22:12:09 +0900

    add new & edit function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,41 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task, notice: "タスク 「#{@task.task_name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.task_name}」を更新しました。"
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:task_name, :priority, :status, :user_name)
+  end
+
 end
