d6d7e5e37afb54f6d48a578f8e19239463871231
makoto tsuta <makoto1850@gmail.com>
2020-04-22 15:38:25 +0900

    add faker gem to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,37 @@
-5.times do |n|
-  name = Faker::Company::bs
+#5.times do |n|
+#  name = Faker::Company::bs
 
-  Label.create!(name: name)
-end
+#  Label.create!(name: name)
+#end
+
+User.create!(
+  name: 'test1',
+  email: 'test@example.com',
+  admin: false,
+  password: 'password',
+  password_confirmation: 'password'
+  )
+
+User.create!(
+  name: 'test2',
+  email: 'test2@example.com',
+  admin: false,
+  password: 'password',
+  password_confirmation: 'password'
+  )
+
+User.create!(
+  name: 'admin1',
+  email: 'admin1@example.com',
+  admin: true,
+  password: 'password',
+  password_confirmation: 'password'
+  )
+
+User.create!(
+  name: 'admin2',
+  email: 'admin2@example.com',
+  admin: true,
+  password: 'password',
+  password_confirmation: 'password'
+  )
